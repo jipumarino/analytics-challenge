d2adeb0db5320579f07e85b6a3318ae7ec925a97
Juan Ignacio Pumarino <jipumarino@gmail.com>
2014-08-29 19:07:12 -0400

    Random dataset generation: first version
    - 10_000 items only (would take ~30 minutes for 1_000_000)
    - no hash calculation

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -1,3 +1,40 @@
 class PageView < Sequel::Model
+
+  def self.repopulate_with_random_dataset
+    available_urls = %w(
+      http://apple.com
+      https://apple.com
+      https://www.apple.com
+      http://developer.apple.com
+      http://en.wikipedia.org
+      http://opensource.org
+    )
+
+    available_referrers = %w(
+      http://apple.com
+      https://apple.com
+      https://www.apple.com
+      http://developer.apple.com
+    ) + [nil]
+
+    start_date = 2.months.ago
+    end_date = Time.now
+
+    random_date_generator = lambda { start_date + rand * (end_date - start_date) }
+
+    self.truncate
+
+    db.transaction do
+      10000.times do
+        self.create(
+          url: available_urls.sample,
+          referrer: available_referrers.sample,
+          created_at: random_date_generator.call,
+          hash: ''
+        )
+      end
+    end
+
+  end
   
 end
