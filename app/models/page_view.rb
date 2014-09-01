397aef309dc5cf99839ddf34c9d7c6810e4ba14f
Juan Ignacio Pumarino <jipumarino@gmail.com>
2014-09-01 12:31:30 -0400

    PageView callback for calculating the hash value

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -2,10 +2,20 @@ class PageView < Sequel::Model
 
   PageView.plugin :timestamps
 
+  def before_validation
+    calcluate_hash_value
+    super
+  end
+
   def self.top_urls
     select_group{ [Sequel.as(date(created_at), date), url] }.
     select_append{ Sequel.as(count(id), visits) }.
     where { date(created_at) > date(Sequel.date_sub(now(0), days: 5))}
+  end
+
+  def calcluate_hash_value
+    values = self.values.slice(:id, :url, :referrer, :created_at).reject{|_,v| v.nil?}
+    self.hash = Digest::MD5.hexdigest(values.to_s)
   end
 
   def self.repopulate_with_random_dataset
