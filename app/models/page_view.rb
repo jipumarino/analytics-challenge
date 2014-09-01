948b597f224f82c39f05d4d99c92a82815b435d5
Juan Ignacio Pumarino <jipumarino@gmail.com>
2014-09-01 13:29:20 -0400

    PageView::top_urls_for_day

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -13,6 +13,15 @@ class PageView < Sequel::Model
     where { created_at > 4.days.ago.to_date }
   end
 
+  def self.top_urls_for_day(date = Date.today, top_limit = 10)
+    select_group{ url }.
+    select_append{ Sequel.as(count(id), visits) }.
+    where { created_at > date }.
+    where { created_at < date+1 }.
+    reverse_order(:visits).
+    limit(top_limit)
+  end
+
   def calcluate_hash_value
     values = self.values.slice(:id, :url, :referrer, :created_at).reject{|_,v| v.nil?}
     self.hash = Digest::MD5.hexdigest(values.to_s)
